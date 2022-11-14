b170696bdb04e256813948a92bc0e1df39629f92
samuel <l17270629@tuxtla.tecnm.mx>
2022-11-13 18:03:51 -0600

    actualizacion

--- cubicoproject/cubicoprojectapp/views.py
+++ cubicoproject/cubicoprojectapp/views.py
@@ -5,20 +5,20 @@ from django.shortcuts import render
 # Create your views here.
 def index(request): #primera vista
 
-    return render(request,"index.html")
+    return render(request,"cubicoprojectapp/index.html")
 
 
 def nosotros(request): #segunda vista
 
-    return render(request,"nosotros.html")
+    return render(request,"cubicoprojectapp/nosotros.html")
 
     
 
 def contacto(request): #tercera vista
 
-    return render(request,"contacto.html")
+    return render(request,"cubicoprojectapp/contacto.html")
 
 
 def projects(request):
-    return render(request,"projects.html") #faltaría un tercer argumento con el contexto(diccionario)
+    return render(request,"cubicoprojectapp/projects.html") #faltaría un tercer argumento con el contexto(diccionario)
  
