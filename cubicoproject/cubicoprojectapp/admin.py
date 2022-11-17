e8795b060d9ad71acfda6b8493d29d6222aebc32
samuel <l17270629@tuxtla.tecnm.mx>
2022-11-16 19:17:48 -0600

    Inserción de imagenes en formato slider
    Listo en mobile y cambia desde tablet para tablet y desktop

--- cubicoproject/cubicoprojectapp/admin.py
+++ cubicoproject/cubicoprojectapp/admin.py
@@ -8,8 +8,7 @@ class GaleriaAdmin(admin.ModelAdmin):
     list_display=("nombre","descripcion") #Lo que debe mostrar en el listado de la tabla
     search_fields=("nombre","descripcion") #Puede filtrar busquedas en relación a..
     readonly_fields=('created','updated') #Solo lectura
- 
-  #  list_filter=("fechasubida",)
+    list_filter=("created","updated") #filtro por creación y actualización
 
 # Modelos aquí.
 admin.site.register(galeria_proyectos, GaleriaAdmin) #brinda acceso al modelo desde el panel y usa la clase creada GaleriaAdmin
--- cubicoproject/cubicoprojectapp/models.py
+++ cubicoproject/cubicoprojectapp/models.py
@@ -6,8 +6,8 @@ class galeria_proyectos(models.Model):
     imagen=models.ImageField(verbose_name="Imagen del proyecto")
     nombre=models.CharField(max_length=100,verbose_name="Nombre del proyecto")
     descripcion=models.CharField(max_length=500,verbose_name="descripción general")
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now_add=True)
+    created=models.DateTimeField(auto_now_add=True,verbose_name="Creación")
+    updated=models.DateTimeField(auto_now_add=True,verbose_name="Modificación")
 
     def __str__(self):
         return self.nombre #Método Devuelve el "nombre" del proyecto para utilizarlo cmo titulo (listado y vista individual)
